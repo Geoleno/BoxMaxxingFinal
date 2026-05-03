5ce470b4f4bddf9731ced616043d5d5547a9a6a1
Geoleno <geoleno20@gmail.com>
2026-05-03 09:31:51 +0700

    Initial Commit

---
+++ BoxMaxxingFinal/BoxMaxxingFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxMaxxingFinalApp.swift
+//  BoxMaxxingFinal
+//
+//  Created by Michael V Ginting on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BoxMaxxingFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxMaxxingFinalTests/BoxMaxxingFinalTests.swift
@@ -0,0 +1,19 @@
+//
+//  BoxMaxxingFinalTests.swift
+//  BoxMaxxingFinalTests
+//
+//  Created by Michael V Ginting on 03/05/26.
+//
+
+import Testing
+@testable import BoxMaxxingFinal
+
+struct BoxMaxxingFinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
